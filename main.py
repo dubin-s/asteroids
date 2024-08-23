bbd263b5e67294c173edf4b564013f376c52ae66
Spenser <18bf4e7@proton.me>
2024-08-23 01:22:00 -0400

    pygame update for tick rate

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -14,6 +16,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        dt = game_clock.tick(60) / 1000
 
 
 if __name__ == '__main__':
